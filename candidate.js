4715a75486c51e57e023eb57b222925fbf2a4e4a
Rodolfo Augusto <rof.bra@gmail.com>
2020-10-31 14:11:36 -0300

    Parte de vereadores finalizada

---
+++ candidate.js
@@ -0,0 +1,50 @@
+let = candidate = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos:[
+            {
+                numero: 38111,
+                nome: 'Zé da venda',
+                partido: LDR,
+                fotos:[
+                    {url: 'v1.jpeg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: 77666,
+                nome: 'Capitão Nascimento',
+                partido: 'MLCI',
+                fotos:[
+                    {url:nascimento.jpeg, legenda: 'Vereador', small: false}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Ferrando SP',
+                partido: 'BST',
+                vice: 'Ngm',
+                fotos:[
+                    {url:'corno.png', legenda: 'Prefeito'},
+                    {url:'v2.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Super Xandão',
+                partido: 'BIRRL',
+                vice: 'Stronda',
+                fotos:[
+                    {url: 'xandao.jpeg', legenda: 'Prefeito'},
+                    {url:'stronda.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
